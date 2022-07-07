de6f5ff241cb3bfbaf102a8504c7c8a38ccd58f0
Latoya Boland <85142599+BolandTech@users.noreply.github.com>
2022-07-07 17:40:22 +0000

    Reorganized programming folder 3...created sub folders for Extras in day 1 & 2 folders...renamed Day 2 file

---
+++ programming/5/Main.java
@@ -0,0 +1,13 @@
+class Main {
+
+  public ststic void main(String[] args) {
+
+    //Grade g = new Grade("3456", 95, 0.10);
+    //g.showInfo();
+
+    Grade g =n new Grade();
+    g.enterInfo();
+    g.showInfo();
+  }
+  
+}
