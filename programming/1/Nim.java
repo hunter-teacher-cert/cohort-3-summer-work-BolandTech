96abc85c1cbce471528958e57c6511a64b335b91
Latoya Boland <85142599+BolandTech@users.noreply.github.com>
2022-07-09 02:45:52 +0000

    added comments to Nim.java file in subfolder 1

--- programming/1/Nim.java
+++ programming/1/Nim.java
@@ -9,7 +9,7 @@ public class Nim
     {
       int stones = 12;
       int stonesTaken;
-
+  //prompt user input (user turn)
       Scanner input = new Scanner(System.in);
         Random rand = new Random();
         while(stones > 0) {
@@ -20,37 +20,31 @@ public class Nim
             stonesTaken = input.nextInt();
         }
             stones -= stonesTaken;
+  //check for did you win
         if (stones <= 0) {
             System.out.println("Player wins");
             break;
         }
+  //calculating number of stones remining and print it
             System.out.println(stones + " Remaining");
-        //computer turn
+  //computer turn...machine takes turn
             stonesTaken = rand.nextInt(3)+1;
             System.out.println("The computer took " + stonesTaken + " stones");
+  //check for did computer win
             if (stonesTaken>stones) {
                 stonesTaken=stones;
             }
             stones -= stonesTaken;
+
             if (stones <= 0) {
             System.out.println("Computer wins");
                 break;
         }
+
+  //calculating number of stones remining and print it
             System.out.println(stones + " Remaining");
-            
-        //prompt user input (user turn)
 
-        //calculating number of stones remining and print it
-
-        //check for did you win
-
-        //machine takes turn
-
-        //calculating number of stones remining and print it
-
-        
-        //check win
-//1 stone remaining to win or select the last 3?
+//*1 stone remaining to win or select the last 3?*//
         
       }
     }
